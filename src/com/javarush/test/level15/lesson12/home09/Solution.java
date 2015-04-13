a14616e29a6c198d8bff23aabf7e65a7ec17ec1c
Lukas <lukas007@bk.ru>
2015-04-13 15:04:02 +0300

    noniy commit

--- src/com/javarush/test/level15/lesson12/home09/Solution.java
+++ src/com/javarush/test/level15/lesson12/home09/Solution.java
@@ -22,16 +22,70 @@ obj name
 double 3.14
 */
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Solution {
-    public static void main(String[] args) {
-        //add your code here
-    }
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String url = scanner.nextLine();
+		String fullUrl = url;
+		int a = 0;
+		ArrayList<String> arrayList = new ArrayList<String>();
+		int nachalo = 0;
+		String subString = null;
 
-    public static void alert(double value) {
-        System.out.println("double " + value);
-    }
+		while (url.contains("=")) {
+			if (url.contains("?")) {
+				nachalo = url.indexOf('?');
+			} else if (url.contains("&")) {
+				nachalo = url.indexOf('&');
+			}
+			subString = url.substring(nachalo + 1, url.indexOf('='));
+			if (subString.contains("&")) {
+				arrayList.add(subString.substring(0, subString.indexOf('&')));
+				arrayList.add(subString.substring(subString.indexOf('&') + 1));
 
-    public static void alert(String value) {
-        System.out.println("String " + value);
-    }
+			} else {
+				arrayList.add(subString);
+			}
+
+			url = url.substring(url.indexOf('=') + 1);
+		}
+
+		for (int i = 0; i < arrayList.size(); i++) {
+			System.out.print(arrayList.get(i) + " ");
+		}
+
+		System.out.println();
+
+		nachalo = fullUrl.indexOf('?') + 1;
+
+
+		String str = null;
+		while (fullUrl.substring(nachalo).contains("obj=")) {
+			nachalo = 0;
+			fullUrl = fullUrl.substring(fullUrl.indexOf("obj=") + 1);
+
+			if (fullUrl.contains("&")) {
+				str = fullUrl.substring(3, fullUrl.indexOf('&'));
+			} else {
+				str = fullUrl.substring(3);
+			}
+			if (str.contains(".")) {
+				alert(Double.parseDouble(str));
+			} else {
+				alert(str);
+			}
+		}
+	}
+
+
+	public static void alert(double value) {
+		System.out.println("double " + value);
+	}
+
+	public static void alert(String value) {
+		System.out.println("String " + value);
+	}
 }
